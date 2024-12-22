ae70168197768cb2701d58291334b42f03423d4c
Soumya ranjan Purohit <91987369+Shinu07@users.noreply.github.com>
2024-12-22 21:17:23 +0530

    #

---
+++ 8kyu/sumArray.js
@@ -0,0 +1,24 @@
+/*Write a function that takes an array of numbers and returns the sum of the numbers. 
+The numbers can be negative or non-integer. If the array does not contain any 
+numbers then you should return 0.
+
+Examples
+Input: [1, 5.2, 4, 0, -1]
+Output: 9.2
+
+Input: []
+Output: 0
+
+Input: [-2.398]
+Output: -2.398
+*/
+
+function sumArrays(arraynum){
+
+    const res = arraynum.reduce((acc,curr)=>acc + curr,0)
+    console.log(res)
+    return res
+}
+
+sumArrays([1, 5.2, 4, 0, -1])
+
